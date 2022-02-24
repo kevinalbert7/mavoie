fcb336b61f8ef23f4a256cb1f48a02658babcd20
kevinalbert7 <20ko@live.fr>
2022-02-24 16:30:11 +0100

    etape1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import styled from 'styled-components'
 
-function App() {
+import { GiShop } from 'react-icons/gi'
+
+const Nav = styled.div`
+  height: 60px;
+  width: 100vw;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  background-color: #85E533;
+  h1 {
+    margin-left: 100px;
+  }
+`
+const Body = styled.div`
+  margin-top: 60px;
+`
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Nav>
+          <GiShop />
+          <h1>Green Shop</h1>
+        </Nav>
       </header>
+
+      <Body>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>Milk</div>
+            <div className='col'>Out of Stock</div>
+            <div className='col'>3.5 MAD</div>
+          </div>
+          <div className='row'>
+            <div className='col'>Bread</div>
+            <div className='col'></div>
+            <div className='col'></div>
+          </div>
+          <div className='row'>
+            <div className='col'>Orange Juice</div>
+            <div className='col'></div>
+            <div className='col'></div>
+          </div>
+        </div>
+      </Body>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
